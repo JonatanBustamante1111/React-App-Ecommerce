b0a54dba359c0fae026e60cb761e6456b31eafe3
JonatanBustamante1111 <jonatanbustamante1111@gmail.com>
2022-11-04 16:43:32 -0300

    guardo cards

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,19 +1,37 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import "../assets/styles/CardComponent.css"
 
+// funcion que renderiza
 function CardComponent(props) {
   return (
-    <Card className='p-2 gap' style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={props.img} />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text>
-          {props.description}
-        </Card.Text>
-        <Card.Title>{props.price}</Card.Title>
-        <Button className='' variant="primary">Agregar</Button>
-      </Card.Body>
-    </Card>
+    <>
+    <figure class="snip1268">
+  <div className="image">
+    <img  src={props.img} alt="sq-sample4"/>
+    <div className="icons">
+      <a href="#"><i class="ion-star"></i></a>
+      <a href="#"> <i class="ion-share"></i></a>
+      <a href="#"> <i class="ion-search"></i></a>
+    </div>
+    <a href="#" class="add-to-cart">Agregar a Carrito</a>
+  </div>
+  <figcaption>
+    <h2>{props.name}</h2>
+    <p>{props.description}</p>
+    <div className="price">{props.price}</div>
+  </figcaption>
+</figure>
+</>
+    // <Card className='p-2 gap' style={{ width: '18rem' }}>
+    //   <Card.Img variant="top" src={props.img} />
+    //   <Card.Body>
+    //     <Card.Title>{props.title}</Card.Title>
+    //     <Card.Text>
+    //       {props.description}
+    //     </Card.Text>
+    //     <Card.Title>{props.price}</Card.Title>
+    //     <Button className='' variant="primary">Agregar</Button>
+    //   </Card.Body>
+    // </Card>
   );
 }
 
