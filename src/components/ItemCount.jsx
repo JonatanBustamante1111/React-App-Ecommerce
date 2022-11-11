7b81a2e2f98dc11301f733b78fe605499e867369
JonatanBustamante1111 <jonatanbustamante1111@gmail.com>
2022-11-10 22:34:01 -0300

    guardo cambios

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,8 +1,7 @@
-
 const ItemCount = () => {
     return (
         <div className="d-flex gap-3 justify-content-center">
-        <input className="w-25" id="inputCantidad" type="number" min="1" value="1"></input>
+        <input className="w-25" id="inputCantidad" type="number" min="1" value="1" ></input>
         <button className="btn btn-primary">ADD TO CART</button>
         </div>
     ) 
